010247e1a6268d76494b97a62f239d1eadd3d73b
Hehong666 <hehong666@outlook.com>
2021-05-05 10:59:10 +0800

    把本地工程添加到git

---
+++ git-test/src/git/FirstClass.java
@@ -0,0 +1,16 @@
+package git;
+
+/**
+ * @Description: TODO
+ * @author: scott
+ * @date: 2021年05月05日 10:50
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+    }
+}
